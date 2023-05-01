c40399dc3d5415348d8c5cf3d0c4470e4f829f86
Parthik07 <parthik.pankhaniya@ashokinfotech.in>
2023-05-01 11:47:43 +0530

    new  vendor page

--- DellyShopApp/DellyShopApp/Views/Pages/VendorLoginPage.xaml.cs
+++ DellyShopApp/DellyShopApp/Views/Pages/VendorLoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;using DellyShopApp.Languages;using DellyShopApp.Models;using DellyShopApp.Services;using Xamarin.Essentials;using Xamarin.Forms.Xaml;namespace DellyShopApp.Views.Pages{    [XamlCompilation(XamlCompilationOptions.Compile)]    public partial class VendorLoginPage    {        public int orgId = Convert.ToInt32(SecureStorage.GetAsync("OrgId").Result);        public VendorLoginPage()        {            InitializeComponent();        }        private async void LoginButtonClick(object sender, EventArgs e)        {            var login = new Login            {                email = email.Text,                password = pswd.Text,                org_Id = orgId,
+﻿using System;using DellyShopApp.Languages;using DellyShopApp.Models;using DellyShopApp.Services;using DellyShopApp.Views.TabbedPages;
+using Xamarin.Essentials;using Xamarin.Forms.Xaml;namespace DellyShopApp.Views.Pages{    [XamlCompilation(XamlCompilationOptions.Compile)]    public partial class VendorLoginPage    {        public int orgId = Convert.ToInt32(SecureStorage.GetAsync("OrgId").Result);        public VendorLoginPage()        {            InitializeComponent();        }        private async void LoginButtonClick(object sender, EventArgs e)        {            var login = new Login            {                email = email.Text,                password = pswd.Text,                org_Id = orgId,
                 type = "vendor"
             };
             if (email.Text == null || email.Text == "")
@@ -23,7 +24,7 @@
                 await DisplayAlert("Opps", "InValid Username or Password", "ok");
                 return;
             }
-            await Navigation.PushAsync(new Venderdata());
+            await Navigation.PushAsync(new VendorsFirstPage());
             await Xamarin.Essentials.SecureStorage.SetAsync("VendorUserId", userDetail.VendorUserId.ToString());
 
         }        private void BackButton(object sender, EventArgs e)        {            Navigation.PopAsync();        }        private async void ForgetPassClick(object sender, EventArgs e)        {            string result = await DisplayPromptAsync(AppResources.ForgotPass,                AppResources.EnterEmailAddress);            if (string.IsNullOrEmpty(result)) return;            await DisplayAlert(AppResources.Success,                AppResources.SuccessSendEmail                + " " + result, AppResources.Okay);        }
---
+++ DellyShopApp/DellyShopApp/Views/Pages/VendorsFirstPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Android.Views;
+using DellyShopApp.Views.CustomView;
+using DellyShopApp.Views.Pages;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Android.Views.View;
+
+namespace DellyShopApp.Views.TabbedPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VendorsFirstPage
+    {
+        
+        public int userId = Convert.ToInt32(SecureStorage.GetAsync("UserId").Result);
+        public object Element { get; private set; }
+       
+        public VendorsFirstPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OrderInfoClick(object sender, EventArgs e)
+        {
+                if (!(sender is PancakeView stack)) return;
+                switch (stack.ClassId)
+                {
+                    case "EditProfile":
+                        OpenPage(new Venderdata());
+                        break;
+
+                    case "MyOder":
+                        OpenPage(new MyOrderPage());
+                        break;
+
+                    case "MyFav":
+                        OpenPage(new MyFavoritePage());
+                        break;
+
+                    case "LastView":
+                        OpenPage(new LastViewPage());
+                        break;
+
+                    case "MyComments":
+                        OpenPage(new MyCommentsPage());
+                        break;
+
+                    case "Notifications":
+                        OpenPage(new NotificationPage());
+                        break;
+
+                    case "Settings":
+                        OpenPage(new SettingsPage());
+                        break;
+                }
+        }
+
+        private void OpenPage(Page page)
+        {
+            Navigation.PushAsync(page);
+        }
+        protected void LogOutClick(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+            Xamarin.Essentials.SecureStorage.RemoveAll();
+        }
+
+        protected void LogInClick(object sender, EventArgs args)
+        {
+            OpenPage(new LoginPage());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new VendorLoginPage());
+        }
+    }
+}
