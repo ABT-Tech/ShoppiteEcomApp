568dd13d8634e37c2de9d3b20beff75d6c18a9c9
Parthik07 <parthik.pankhaniya@ashokinfotech.in>
2023-08-02 12:53:48 +0530

    Payment Options Ui Design

--- DellyShopApp/DellyShopApp/Views/Pages/AddNewCardPage.xaml.cs
+++ DellyShopApp/DellyShopApp/Views/Pages/AddNewCardPage.xaml.cs
@@ -24,55 +24,54 @@ namespace DellyShopApp.Views.Pages
             Navigation.PopModalAsync();
         }
 
-        async void ScanCreditCard(System.Object sender, System.EventArgs e)
-        {
-            //try
-            //{
-            //    var result = await CrossPayCards.Current.ScanAsync();
-            //    if (result != null)
-            //    {
-            //        if (!string.IsNullOrEmpty(result.CardNumber))
-            //        {
-            //            CardNumber.Text = result.CardNumber;
-            //        }
-            //         if (!string.IsNullOrEmpty(result.ExpirationDate))
-            //        {
-            //            CardExpirationDate.Text = result.ExpirationDate;
-            //        }
-            //         if (!string.IsNullOrEmpty(result.HolderName))
-            //        {
-            //            CvvEntry.Text = result.HolderName;
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    return;
-            //}
+        //async void ScanCreditCard(System.Object sender, System.EventArgs e)
+        //{
+        //    try
+        //    {
+        //        var result = await CrossPayCards.Current.ScanAsync();
+        //        if (result != null)
+        //        {
+        //            if (!string.IsNullOrEmpty(result.CardNumber))
+        //            {
+        //                CardNumber.Text = result.CardNumber;
+        //            }
+        //            if (!string.IsNullOrEmpty(result.ExpirationDate))
+        //            {
+        //                CardExpirationDate.Text = result.ExpirationDate;
+        //            }
+        //            if (!string.IsNullOrEmpty(result.HolderName))
+        //            {
+        //                CvvEntry.Text = result.HolderName;
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return;
+        //    }
             // If you don't want to be dependent on Paypal, you can use the code above.
 
 
 
             //Optional parameter CardIOLogo("PayPal", "CardIO" or "None") for ScanCard method by default "PayPal" is used
-            var result = await CrossPayPalManager.Current.ScanCard(CardIOLogo.None);
-            if (result.Status == PayPalStatus.Cancelled)
-            {
-                Debug.WriteLine("Cancelled");
-            }
-            else if (result.Status == PayPalStatus.Successful)
-            {
-                if (result.Card.CardImage != null)
-                {
-                    var Source = result.Card.CardImage;
-                }
+            //var result = await CrossPayPalManager.Current.ScanCard(CardIOLogo.None);
+            //if (result.Status == PayPalStatus.Cancelled)
+            //{
+            //    Debug.WriteLine("Cancelled");
+            //}
+            //else if (result.Status == PayPalStatus.Successful)
+            //{
+            //    if (result.Card.CardImage != null)
+            //    {
+            //        var Source = result.Card.CardImage;
+            //    }
 
-                CardNumber.Text = result.Card.CardNumber;
-                CardExpirationDate.Text = $"{result.Card.ExpiryMonth}/{result.Card.ExpiryYear}";
-                CvvEntry.Text = result.Card.Cvv;
-                Debug.WriteLine($"CardNumber: {result.Card.CardNumber}, CardType: {result.Card.CardType.ToString()}, Cvv: {result.Card.Cvv}, ExpiryMonth: {result.Card.ExpiryMonth}");
-                Debug.WriteLine($"ExpiryYear: {result.Card.ExpiryYear}, PostalCode: {result.Card.PostalCode}, RedactedCardNumber: {result.Card.RedactedCardNumber}, Scaned: {result.Card.Scaned}");
-            }
+            //    CardNumber.Text = result.Card.CardNumber;
+            //    CardExpirationDate.Text = $"{result.Card.ExpiryMonth}/{result.Card.ExpiryYear}";
+            //    CvvEntry.Text = result.Card.Cvv;
+            //    Debug.WriteLine($"CardNumber: {result.Card.CardNumber}, CardType: {result.Card.CardType.ToString()}, Cvv: {result.Card.Cvv}, ExpiryMonth: {result.Card.ExpiryMonth}");
+            //    Debug.WriteLine($"ExpiryYear: {result.Card.ExpiryYear}, PostalCode: {result.Card.PostalCode}, RedactedCardNumber: {result.Card.RedactedCardNumber}, Scaned: {result.Card.Scaned}");
+            //}
         }
     }
 
-}
